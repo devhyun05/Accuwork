a8f11a4d1f5811e0a5420e8bf0b564cfd1c2ce80
HyunSeongLee <devhyun05@gmail.com>
2023-10-21 15:29:45 -0400

    login fixed

--- estate_chain_frontend/src/components/forms/login.js
+++ estate_chain_frontend/src/components/forms/login.js
@@ -1,23 +1,18 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Dashboard } from '../layouts/dashboard';
 
 
 export const LoginForm = () => {
-  const navigate = useNavigate()
   const [errorMessage, setErrorMessage] = useState(null);
   const [defaultAccount, setDefaultAccount] = useState(null); 
   const [userBalance, setUserBalance] = useState(null); 
   const [connButtonText, setConnButtonText] = useState('Connect Wallet'); 
 
     const connectWalletHandler = () => {
+      
       if (window.ethereum) {
           window.ethereum.request({method: 'eth_requestAccounts'})
           .then(result => {
-                const data = {
-                    address : result[0],
-                }
-                navigate("/dashboard",{state:data})
+              accountChangedHandler(result[0])
           }); 
       } else {
           setErrorMessage("Install MetaMask")
@@ -25,36 +20,38 @@ export const LoginForm = () => {
   }
 
     const accountChangedHandler = (newAccount) => {
-        setDefaultAccount(newAccount.address); 
-        // getUserBalance(); 
- }
+        setDefaultAccount(newAccount); 
+        getUserBalance(newAccount); 
+    }
 
-    // const getUserBalance = () => {
-    //     window.ethereum.request({method: 'eth_getBalance'})
-    //     .then(address => {
-    //       setUserBalance(address)
-    //     })
-    // };
+    const getUserBalance = (address) => {
+      window.ethereum.request({ method: 'eth_getBalance', params: [address] })
+          .then(balance => {
+              setUserBalance(balance);
+          })
+          .catch(error => {
+              console.error("Error fetching balance:", error);
+          });
+  };
+  
 
 
   return (
     <>
-    <a onClick={connectWalletHandler} className="text-sm font-semibold leading-6 text-gray-900">
-        
+    <a onClick={connectWalletHandler} className="text-sm font-semibold leading-6 text-gray-900">        
              <button type="button" 
-                    class="text-white bg-blue-400 hover:bg-gray-800 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center "
+                    className="text-white bg-blue-400 hover:bg-gray-800 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center "
                     onClick={connectWalletHandler}>
                 {connButtonText}
             </button>
             <span aria-hidden="true">&rarr;</span>
           </a>
-            {/* <div className="accountDisplay">
+             <div className="accountDisplay">
               <h3>Address: {defaultAccount}</h3>
             </div>
             <div className="balanceDisplay">
               <h3>Balance: {userBalance}</h3>
             </div>
-            {errorMessage} */}
             {errorMessage}
     </>
   );
--- estate_chain_frontend/src/components/layouts/landing.js
+++ estate_chain_frontend/src/components/layouts/landing.js
@@ -10,11 +10,24 @@ export const Landing = () => {
     <>
     <body className='min-h-screen bg-gradient-to-r from-yellow-200 via-pink-200 to-pink-400'>
     <Navbar />
-    <div className="flex w-4/6 mx-auto justify-center">
+     <div className='grid grid-cols-2 p-10 m-10'>
 
-     <div className="bg-gray-100  items-center justify-center">
-        
-         </div>
+     <div className="bg-gray-100 items-center justify-center p-10">
+         <h1 className="text-3xl font-semibold mb-4">Accuwork</h1>
+         <p className="text-gray-600 mb-4">
+         Get the proof of work experience
+         </p>
+     </div>
+ 
+     <div className="bg-gray-100 flex items-center justify-center">
+      <div className="bg-white rounded-lg shadow-md p-8 w-full max-w-xl">
+        <div>
+            <label for="website" class="block mb-2 text-sm font-medium text-gray-900 ">Website URL</label>
+            <input type="url" id="website" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="flowbite.com" required/>
+        </div>
+
+      </div>
+     </div>
  </div>
  </body>
  </>
