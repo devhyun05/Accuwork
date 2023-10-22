cb38b186ee936313e0e30c0529cb9cf6253c40b7
HyunSeongLee <devhyun05@gmail.com>
2023-10-22 10:10:30 -0400

    accuwork solidity

--- estate_chain_frontend/src/components/forms/Login.jsx
+++ estate_chain_frontend/src/components/forms/Login.jsx
@@ -1,57 +1,69 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [errorMessage, setErrorMessage] = useState(null);
-  const [defaultAccount, setDefaultAccount] = useState(null); 
-  const [userBalance, setUserBalance] = useState(null); 
-  const [connButtonText, setConnButtonText] = useState('Connect Wallet'); 
-  const navigate = useNavigate(); 
-    const connectWalletHandler = () => {
-      
-      if (window.ethereum) {
-          window.ethereum.request({method: 'eth_requestAccounts'})
-          .then(result => {
-              accountChangedHandler(result[0])
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [userBalance, setUserBalance] = useState(null);
+  const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+  const navigate = useNavigate();
 
-          }); 
-      } else {
-          setErrorMessage("Install MetaMask")
-      }
-  }
-
-    const accountChangedHandler = (newAccount) => {
-        setDefaultAccount(newAccount); 
-        getUserBalance(newAccount); 
+  const connectWalletHandler = () => {
+    if (window.ethereum) {
+      window.ethereum
+        .request({ method: 'eth_requestAccounts' })
+        .then((result) => {
+          accountChangedHandler(result[0]);
+        })
+        .catch((error) => {
+          setErrorMessage(`Error connecting wallet: ${error.message}`);
+        });
+    } else {
+      setErrorMessage("Install MetaMask");
     }
-
-    const getUserBalance = (address) => {
-      window.ethereum.request({ method: 'eth_getBalance' })
-          .then(balance => {
-              setUserBalance(balance);
-              navigate("/dashboard", {state: address});
-          })
-          .catch(error => {
-              console.error("Error fetching balance:", error);
-          });
   };
-  
 
+  const accountChangedHandler = (newAccount) => {
+    setDefaultAccount(newAccount);
+    getUserBalance(newAccount);
+  };
+
+  const getUserBalance = (address) => {
+    window.ethereum
+      .request({ method: 'eth_getBalance', params: [address] })
+      .then((balance) => {
+        setUserBalance(balance);
+        setConnButtonText('Logout');
+        navigate('/', { state: address });
+      })
+      .catch((error) => {
+        setErrorMessage(`Error fetching balance: ${error.message}`);
+        console.error("Error fetching balance:", error);
+      });
+  };
+
+  const logoutHandler = () => {
+    setDefaultAccount(null);
+    setUserBalance(null);
+    setConnButtonText('Connect Wallet');
+    navigate('/');
+  };
 
   return (
     <>
-    <a onClick={connectWalletHandler} className="text-sm font-semibold leading-6 text-gray-900">        
-             <button type="button" 
-                    className="text-white bg-blue-400 hover:bg-gray-800 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center "
-                    onClick={connectWalletHandler}>
-                {connButtonText}
-            </button>
-         
-          </a>
-        
-            {errorMessage}
+      <a className="text-sm font-semibold leading-6 text-gray-900">
+        <button
+          type="button"
+          className="text-white bg-blue-400 hover:bg-gray-800 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center"
+          onClick={defaultAccount ? logoutHandler : connectWalletHandler}
+        >
+          {connButtonText}
+        </button>
+      </a>
+
+      {errorMessage && <p className="text-red-500">{errorMessage}</p>}
     </>
   );
 };
 
-export default Login; 
+export default Login;
