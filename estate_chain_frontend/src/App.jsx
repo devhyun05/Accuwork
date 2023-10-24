349a32b0ea44420fe697fdc5bca97837bc1ce7f1
Julius Dejon <juliusdejon@gmail.com>
2023-10-24 00:14:27 -0400

    ethers

--- estate_chain_frontend/src/App.jsx
+++ estate_chain_frontend/src/App.jsx
@@ -1,21 +1,32 @@
-// import { Dashboard } from "./components/layouts/Dashboard";
-// import { Landing } from "./components/layouts/Landing";
-// import { WorkExperience } from "./components/layouts/Workexperience";
-// import { Search } from "./components/layouts/Search";
-// import { Share } from "./components/layouts/Share"; 
+import Dashboard from "pages/Dashboard";
+import WorkExperience from "components/layouts/Workexperience";
+import Search from "components/layouts/Search";
+import Navbar from "components/layouts/Navbar";
+import Share from "components/layouts/Share";
 
-import Dashboard from './components/layouts/Dashboard';
-import Landing from './components/layouts/Landing';
-import WorkExperience from './components/layouts/Workexperience';
-import Search from './components/layouts/Search';
-import Share from './components/layouts/Share';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
+function Landing() {
+  return (
+    <div>
+      <Navbar />
+      <div
+        style={{
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#2c3e50",
+          flexDirection: "column",
+          color: "white",
+        }}
+      >
+        Landing Page coming soon...
+      </div>
+    </div>
+  );
+}
 function App() {
   return (
     <div>
--- estate_chain_frontend/src/context/TransactionContext.jsx
+++ estate_chain_frontend/src/context/TransactionContext.jsx
@@ -1,38 +1,42 @@
-import React, { useState, useEffect } from 'react'; 
-import { ethers } from 'ethers'; 
-import { contractABI, contractAddress} from '../../utils/constants'; 
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { contractABI, contractAddress } from "utils/constants";
 
 export const TransactionContext = React.createContext();
 
-const { ethereum } = window; 
+const { ethereum } = window;
 
 const getEthereumContract = () => {
-  const provider = new ethers.providers.Web3Provider(ethereum); 
-  const signer = provider.getSigner(); 
-  const transactionContract = new ethers.Contract(contractAddress, contractABI, signer); 
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const transactionContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
 
   console.log({
     provider,
-    signer, 
-    transactionContract
-  }); 
-}
+    signer,
+    transactionContract,
+  });
+};
 
 export const TransactionProvider = ({ children }) => {
-    const checkIfWalletIsConnected = async () => {
-        if (!ethereum) return alert("Please install metamask"); 
-        
-        const accounts = await ethereum.request({ method: 'eth_accounts'}); 
+  const checkIfWalletIsConnected = async () => {
+    if (!ethereum) return alert("Please install metamask");
 
-        console.log(accounts); 
-    }
+    const accounts = await ethereum.request({ method: "eth_accounts" });
 
-    useEffect(() =>{
-        checkIfWalletIsConnected();
-    }, []);
-    return (
-        <TransactionContext.Provider value={{ value: 'test' }}>
-            {children}
-        </TransactionContext.Provider>
-    )
-}
+    console.log(accounts);
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+  return (
+    <TransactionContext.Provider value={{ value: "test" }}>
+      {children}
+    </TransactionContext.Provider>
+  );
+};
