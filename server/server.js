0005afae1203e67b58cb0f27593117db85ee70ef
HyunSeongLee <devhyun05@gmail.com>
2023-10-28 14:11:19 -0400

    installed cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000; 
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/contracts', express.static(path.join(__dirname, './artifcats'))); 
